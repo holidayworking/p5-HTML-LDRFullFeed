4667ed711fd641a27fe54473d3532c26fe60ee7f
Hidekazu Tanaka <hidekazu.tanaka@gmail.com>
2011-06-20 23:09:28 +0900

    initial commit

---
+++ lib/HTML/LDRFullFeed.pm
@@ -0,0 +1,32 @@
+package HTML::LDRFullFeed;
+use strict;
+use warnings;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=head1 NAME
+
+HTML::LDRFullFeed -
+
+=head1 SYNOPSIS
+
+  use HTML::LDRFullFeed;
+
+=head1 DESCRIPTION
+
+HTML::LDRFullFeed is
+
+=head1 AUTHOR
+
+Hidekazu Tanaka E<lt>hidekazu.tanaka@gmail.comE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
